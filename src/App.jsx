7b067594c1e4bd6a5b8a469829f3cfac537b89d1
ChengZirao <orange_r@outlook.com>
2024-03-07 15:28:54 +0800

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+// Create App.jsx template by type 'rfc'
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+// (To use this shortcut, need to download 'Tailwind CSS IntelliSense' extension first)
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <div className=" text-red-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/sign-in"
+          element={<SignIn />}
+        />
+        <Route
+          path="/sign-up"
+          element={<SignUp />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/profile"
+          element={<Profile />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }

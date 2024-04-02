4f39422c37eb9f90210dd34f752885e244aad519
ChengZirao <orange_r@outlook.com>
2024-04-02 17:19:42 +0800

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(`${err.name}: ${err.message}`);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./config.env" });
+const DBAtlas = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DBAtlas).then((connection) => {
+  console.log("DB connection successful!");
+});
 
 const app = express();
 
-app.listen(3333, () => console.log("server running"));
+app.listen(3333, () => console.log("Server is running!"));

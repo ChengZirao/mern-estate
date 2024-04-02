9183c1902cc6b3cac242b6622f77f9f5d7be86f7
ChengZirao <orange_r@outlook.com>
2024-04-02 15:34:55 +0800

    Create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 // Create App.jsx template by type 'rfc'
+// (To use this shortcut, need to download 'Tailwind CSS IntelliSense' extension first)
 
+import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
-// (To use this shortcut, need to download 'Tailwind CSS IntelliSense' extension first)
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* The Header component will be at the top of all pages */}
+      <Header />
       <Routes>
         <Route
           path="/"
